17945d1bf722b91961308438926936b6216867e3
verzac <benjamin.tanone@gmail.com>
2020-03-02 11:14:18 +1100

    cosmetic update on FreeShippingListingPercentage and index.html (edited page title)

--- packages/frontend/src/components/datacards/FreeShippingListingPercentageDataCard.tsx
+++ packages/frontend/src/components/datacards/FreeShippingListingPercentageDataCard.tsx
@@ -4,6 +4,6 @@ import DataCard, { ProductDataCardProp } from "./SimpleDataCard";
 export default function(props: ProductDataCardProp) {
   const { products } = props;
   let productsWithFreeShipping = products.filter(product => product.freeShipping);
-  let percentageString: string = (productsWithFreeShipping.length / products.length * 100).toFixed(2) + '%';
+  let percentageString: string = Math.floor(productsWithFreeShipping.length / products.length * 100) + '%';
   return <DataCard label="% of listings with free shipping" data={percentageString} />;
 }
